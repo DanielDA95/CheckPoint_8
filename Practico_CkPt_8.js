56ab0060c15f170e43607f907ad4494a2d260645
DanielDA95 <126833141+DanielDA95@users.noreply.github.com>
2024-05-19 11:34:15 +0200

    Create Practico_CkPt_8.js

---
+++ Practico_CkPt_8.js
@@ -0,0 +1,19 @@
+// Primer ejercicio. 
+let miLista = ["velma", "exploradora", "jane", "john", "harry"];
+
+for (let i = 0; i < miLista.length; i++) {
+    console.log(miLista[i]);
+}
+
+//Segundo ejercicio practico.
+let miLista = ["velma", "exploradora", "jane", "john", "harry"];
+let contador = 0;
+
+while (contador < miLista.length) {
+    console.log(miLista[contador]);
+    contador++;
+}
+
+//Tercer punto practico
+const holaMundo = () => "Hola mundo";
+console.log(holaMundo());
